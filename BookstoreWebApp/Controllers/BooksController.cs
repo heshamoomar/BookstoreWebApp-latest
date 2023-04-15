e84e8e70496bbc64b41965eb21e41cbc2310fa0e
Hesham Omar <hesham.omar1122@gmail.com>
2023-04-15 15:55:02 +0200

    Add project files.

---
+++ BookstoreWebApp/Controllers/BooksController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookstoreWebApp.Data;
+using BookstoreWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookstoreWebApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+              return _context.Book != null ? 
+                          View(await _context.Book.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+        }
+
+        // GET: Books/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+              return _context.Book != null ? 
+                          View() :
+                          Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+        }
+
+        // POST: Books/ShowFilterResults
+        public async Task<IActionResult> ShowFilterResults(String Author)
+        {
+              return _context.Book != null ?
+                          View("Index", await _context.Book.Where(a => a.Author.Contains(Author)).ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Author,AuthorUrl")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Author,AuthorUrl")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Book == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+                _context.Book.Remove(book);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+          return (_context.Book?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
